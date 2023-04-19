a2ebc71c8c0968f3309dec8d5c4ce1acce4fa43b
AYAME <12324265+4Y4M3@users.noreply.github.com>
2023-04-19 15:59:34 +0900

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -1,6 +1,7 @@
 from get_chrome_driver import GetChromeDriver
 from selenium import webdriver
 
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
