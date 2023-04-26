1ea3f42e401b67efa89d0c5073c38c1452a5d5aa
AYAME <12324265+4Y4M3@users.noreply.github.com>
2023-04-26 11:13:33 +0900

    Update main.py

--- selenium/main.py
+++ selenium/main.py
@@ -17,12 +17,12 @@ driver = driver_init()
 
 wait = WebDriverWait(driver=driver, timeout=30)
 
-driver.get('https://www.fmyokohama.co.jp/program/listentomydark')
+driver.get('https://twitter.com/ltmd847')
 
 # 要素が全て検出できるまで待機する
 wait.until(EC.presence_of_all_elements_located)
 
-result = driver.find_element(By.XPATH, '//*[@id="container"]/main/div[3]/div[2]/span').text
+result = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/section/div/div').text
 
 print(driver.current_url)
 print(result)
